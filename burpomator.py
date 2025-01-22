47928889917aa7866bd19999c611ae894380b7c1
Allen Montgomery <15912502+iosecurityio@users.noreply.github.com>
2025-01-22 17:45:59 -0500

    .env

--- burpomator.py
+++ burpomator.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 class Scanner:
     """Creates a BURP Scanner Object to interact with Professional REST API"""
 
-    load_dotenv("./env")
+    load_dotenv("./.env")
 
     def __init__(self):
         self.api_key = os.getenv("API_KEY")
@@ -29,6 +29,8 @@ class Scanner:
         response = requests.post(endpoint, json=data, headers=headers)
         if response.status_code == 201:
             print("Scan Successfully Started")
+        elif response.status_code == 401:
+            print("Unauthorized. Check API_KEY or .env")
         else:
             print("Scan Failed to Start: Code", response.status_code)
             print(response.json().get("error"))
@@ -38,6 +40,6 @@ if __name__ == "__main__":
     # Create a Scanner object
     scanner = Scanner()
     # Define a target
-    TARGET = "http://localhost:3000"
+    TARGET = "http://127.0.0.1:3000"
     # Start the scan
     scanner.scan(urls=TARGET)
